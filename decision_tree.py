c46606c329c83b02fc247ca8102209d1d9353128
jorniklenderlyn <khcfksiirjfldik@gmail.com>
2025-10-30 22:49:24 +0300

    cover all tests but failur on tests that asses accuracy

--- decision_tree.py
+++ decision_tree.py
@@ -1,5 +1,138 @@
+import numpy as np
+import logging 
+
+logging.basicConfig(filename='.logs', filemode='w', format='%(asctime)s - %(message)s', encoding='utf-8', level=logging.INFO)
+
+
 class Node:
-    ...
+    def __init__(self, feature=None, treshold=None, left=None, right=None, *, value=None):
+        self.feature = feature
+        self.treshold = treshold 
+        self.left = left 
+        self.right = right 
+        self.value = value 
+    
+    def is_leaf(self):
+        return self.value is not None
 
 class DecisionTree:
-    ... 
+    def __init__(self, max_depth=100, min_samples_split=2, criterion='gini'):
+        self.max_depth = max_depth
+        self.min_saples_split = min_samples_split
+        self.criterion = criterion
+        self.root = None
+    
+    def _gini(self, y):
+        if len(y) == 0:
+            return 0
+        counts = np.bincount(y)
+        probabilities = counts / len(y)
+        return 1 - np.sum(probabilities**2)
+    
+    def _entropy(self, y):
+        if len(y) == 0:
+            return 0
+        counts = np.bincount(y)
+        probabilities = counts / len(y)
+        return -np.sum(probabilities * np.log2(probabilities))
+    
+    def _information_gain(self, y, y_left, y_right, criterion):
+        if criterion == 'gini':
+            impurity_func = self._gini
+        elif criterion == 'entropy':
+            impurity_func = self._entropy 
+        
+        parent_impurity = impurity_func(y)
+
+        # Weighted average of child impurities
+        n = len(y)
+        n_left, n_right = len(y_left), len(y_right)
+
+        if n == 0:
+            return 0
+        
+        child_impurity = n_left / n * impurity_func(y_left) + n_right / n * impurity_func(y_right)
+
+        return parent_impurity - child_impurity
+    
+    def _best_split(self, X, y):
+        best_gain = -1
+        best_treshold = None
+        best_feature = None
+
+        n_samples, n_features = X.shape
+
+        logging.info(f'_best_split: {n_samples} {self.min_saples_split}')
+
+        if n_samples < self.min_saples_split:
+            return best_feature, best_treshold
+
+        for feature_idx in range(n_features):
+            feature_values = np.unique(X[:, feature_idx])
+
+            for i in range(len(feature_values) - 1):
+                treshold = (feature_values[i] - feature_values[i + 1]) / 2
+
+                left_mask = X[:, feature_idx] <= treshold
+                right_mask = X[:, feature_idx] > treshold
+
+                y_left = y[left_mask]
+                y_right = y[right_mask]
+
+                if len(y_left) == 0 or len(y_right) == 0:
+                    continue
+
+                gain = self._information_gain(y, y_left, y_right, criterion=self.criterion)
+
+                if best_gain < gain:
+                    best_gain = gain
+                    best_treshold = treshold
+                    best_feature = feature_idx
+        logging.info(f'_best_split: {best_feature} {best_treshold}')
+        
+        return best_feature, best_treshold
+    
+    def _build_tree(self, X, y, depth=0) -> Node:
+        n_samples, n_features = X.shape
+
+        if depth >= self.max_depth or n_samples < self.min_saples_split or len(np.unique(y)) == 1:
+            values, counts = np.unique(y, return_counts=True)
+            index_of_mode = np.argmax(counts)
+            most_common = values[index_of_mode]
+            return Node(value=most_common)
+        
+        best_feature, treshold = self._best_split(X, y)
+
+        logging.info(str(best_feature) + ' ' + str(treshold))
+
+        if best_feature is None:
+            values, counts = np.unique(y, return_counts=True)
+            index_of_mode = np.argmax(counts)
+            most_common = values[index_of_mode]
+            return Node(value=most_common)
+
+        left_mask = X[:, best_feature] <= treshold
+        right_mask = X[:, best_feature] > treshold
+
+        left_subtree = self._build_tree(X[left_mask], y[left_mask], depth=depth + 1)
+        right_subtree = self._build_tree(X[right_mask], y[right_mask], depth=depth + 1)
+
+        return Node(value=treshold, feature=best_feature, left=left_subtree, right=right_subtree)
+
+    def fit(self, X, y):
+        self.root = self._build_tree(X, y)
+        self.n_classes = len(np.unique(y))
+        self.n_features = X.shape[1]
+
+    def _predict_single(self, x, node):
+        if node.is_leaf():
+            return node.value
+        
+        if x <= node.treshold:
+            return self._predict_single(x, node.left)
+        else:
+            return self._predict_single(x, node.right)
+        
+    
+    def predict(self, X):
+        return np.array([self._predict_single(x, self.root) for x in X])
--- test_decision_tree.py
+++ test_decision_tree.py
@@ -2,6 +2,7 @@ import unittest
 import numpy as np 
 from sklearn.datasets import load_iris 
 from sklearn.model_selection import train_test_split 
+from sklearn.metrics import accuracy_score
 from decision_tree import DecisionTree 
 
 
@@ -16,6 +17,67 @@ class TestDecisionTreeIris(unittest.TestCase):
             self.X, self.y, test_size=0.2, random_state=42, stratify=self.y
         )
     
+    # testing implementation details
+    def test_gini_pure(self):
+        y = np.array([1, 1, 1])
+        tree = DecisionTree()
+        self.assertAlmostEqual(tree._gini(y), 0.0)
+    
+    def test_gini_mixed(self):
+        y = np.array([0, 1, 0, 1])
+        tree = DecisionTree()
+        self.assertAlmostEqual(tree._gini(y), 0.5)
+    
+    def test_entropy_pure(self):
+        y = np.array([1, 1, 1])
+        tree = DecisionTree()
+        self.assertAlmostEqual(tree._entropy(y), 0.0)
+    
+    def test_entropy_max(self):
+        y = np.array([0, 1, 0, 1])
+        tree = DecisionTree()
+        self.assertAlmostEqual(tree._entropy(y), 1.0)
+    
+    def test_information_gain_gini(self):
+        tree = DecisionTree()
+        y = np.array([0, 0, 1, 1])
+        y_left = np.array([0, 0])
+        y_right = np.array([1, 1])
+        information_gain = tree._information_gain(y, y_left, y_right, criterion='gini')
+        self.assertEqual(information_gain, 0.5)
+    
+    def test_information_gain_entropy(self):
+        tree = DecisionTree()
+        y = np.array([0, 0, 1, 1])
+        y_left = np.array([0, 1])
+        y_right = np.array([0, 1])
+        information_gain = tree._information_gain(y, y_left, y_right, criterion='entropy')
+        self.assertEqual(information_gain, 0.0)
+    
+    # end of implemantation details tests
+    
+    def test_iris_gini_accuracy(self):
+        tree = DecisionTree(max_depth=4, min_samples_split=2, criterion='gini')
+        tree.fit(self.X_train, self.y_train)
+        y_pred = tree.predict(self.X_test)
+        score = accuracy_score(self.y_test, y_pred, normalize=True)
+        self.assertGreaterEqual(score, 0.9)
+
+    
+    def test_iris_entropy_accuracy(self):
+        tree = DecisionTree(max_depth=4, min_samples_split=2, criterion='entropy')
+        tree.fit(self.X_train, self.y_train)
+        y_pred = tree.predict(self.X_test)
+        score = accuracy_score(self.y_test, y_pred, normalize=True)
+        self.assertGreaterEqual(score, 0.9)
+    
+    def test_full_fit_perfect_train_score(self):
+        tree = DecisionTree(max_depth=1000000, min_samples_split=2)
+        tree.fit(self.X_train, self.y_train)
+        y_pred = tree.predict(self.X_test)
+        score = accuracy_score(self.y_test, y_pred)
+        self.assertEqual(score, 1.0)
+    
     def test_best_split_is_used_correctly(self):
         X = np.array([[1], [2], [3], [4], [5]])
         y = np.array([0, 0, 1, 1, 1])
@@ -26,6 +88,30 @@ class TestDecisionTreeIris(unittest.TestCase):
         self.assertEqual(root.feature, 0)
         self.assertGreater(root.treshold, 2)
         self.assertLess(root.treshold, 3)
+
+    def test_predict_output_shape(self):
+        tree = DecisionTree()
+        tree.fit(self.X_train, self.y_train)
+        y_pred = tree.predict(self.X_test)
+        self.assertEqual(self.y_test.shape, y_pred.shape)
     
     def test_leaf_values_are_valid_classes(self):
-        ...
+        tree = DecisionTree(max_depth=3)
+        tree.fit(self.X_train, self.y_train)
+        y_pred = tree.predict(self.X_test)
+        unique_preds = np.unique(y_pred)
+        valid_classes = np.unique(self.y_train)
+        for pred in unique_preds:
+            self.assertIn(pred, valid_classes)
+    
+    def test_reproducibility(self):
+        tree1 = DecisionTree()
+        tree2 = DecisionTree()
+        tree1.fit(self.X_train, self.y_train)
+        tree2.fit(self.X_train, self.y_train)
+        pred1 = tree1.predict(self.y_test)
+        pred2 = tree2.predict(self.y_test)
+        np.testing.assert_array_equal(pred1, pred2)
+
+if __name__ == '__main__':
+    unittest.main()
